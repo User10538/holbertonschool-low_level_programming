11d13f8f7320c60a3542e2490e8cd7453d01d150
User10538 <10538@holbertonstudents.com>
2025-02-17 05:10:30 +0000

    Fixes - add print the n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,6 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	printf("%d ", n);  // Print the value of n
 	if (n > 0) {
 	printf("is positive");
 	} else if (n < 0) {
