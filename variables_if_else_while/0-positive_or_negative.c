604fe4f4467f997559ed130c6214db97715d3654
User10538 <10538@holbertonstudents.com>
2025-02-17 04:50:25 +0000

    Fix errors

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,30 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0) {
-	printf("is positive");
-	} else if (n < 0) {
-    printf("is negative");
-	} else {
-    printf("is zero");
-	}
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        printf("%d ", n);  // Print the value of n
+    if (n > 0) {
+    printf("is positive\n");
+    } else if (n < 0) {
+    printf("is negative\n");
+    } else {
+    printf("is zero\n");
+    }
+        return (0);
 }
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                                                                                                                                                                                                                       
+~                                                                   
