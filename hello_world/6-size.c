85fc1d3c16bcecc82d3325693576b72caaa4d6c1
User10538 <10538@holbertonstudents.com>
2025-02-17 03:40:50 +0000

    fixes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,19 +3,18 @@
 /**
  * main - Entry point
  *
- * This is to orint all the sizes of the various types
- * on the computer it is complied and runs on.
+ * Description: This program prints the size of various types
+ * on the computer it is compiled and run on.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf(Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
+	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
 
 	return (0);
-
 }
