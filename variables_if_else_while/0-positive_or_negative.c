b59b20412df78363fa55ebc0562155c22733df45
User10538 <10538@holbertonstudents.com>
2025-02-17 23:25:24 +0000

    Fix Betty Coding style

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
 /**
@@ -10,17 +9,20 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	printf("%d ", n);  
-	if (n > 0) {
-	printf("is positive\n");
-	} else if (n < 0) {
-    printf("is negative\n");
-	} else {
-    printf("is zero\n");
-	}
+	last_digit = n % 10;
+
+	printf("Last digit of %d is %d ", n, last_digit);
+
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
+
 	return (0);
 }
