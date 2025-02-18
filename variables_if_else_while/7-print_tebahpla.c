6caf8c2b924b28b50d73479996f96e8bb448f927
User10538 <10538@holbertonstudents.com>
2025-02-18 12:25:11 +0000

    Use for loop

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single-digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	
+	for(ch='z'; ch >= 'a'; ch--)
+	
+	putchar(ch);
+	
+	putchar('\n');
+	
+	return(0);
+
+}
