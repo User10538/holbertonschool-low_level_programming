c0323a285243f6a01a9836114909e97646c69e45
User10538 <10538@holbertonstudents.com>
2025-02-18 11:44:04 +0000

    use int

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * retun - always 0 (success)
+ */
+
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n<=10; n++)
+		putchar(n+'0');
+
+	putchar('\n');
+
+	return(0);
+
+
+}
