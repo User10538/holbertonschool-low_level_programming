0f6c983487151ea24a8712f43876c6d31ece6600
User10538 <10538@holbertonstudents.com>
2025-02-18 12:02:28 +0000

    Betty coding style fix

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
+	int i;
+    	for (i = 0; i < 10; i++)
         printf("%d", i);
-
-    printf("\n");
-
-    return (0);
+    
+	printf("\n");
+    	return (0);
 }
