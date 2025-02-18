5c419bb45794ddf2c2299f6f3ded57ce77b767d0
User10538 <10538@holbertonstudents.com>
2025-02-18 13:21:18 +0000

    Fix Betty coding

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,16 +7,18 @@
 int main(void)
 {
 	int i;
-    	for (i = 0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 		if (i != 9)
 		{
-	    		putchar(',');
-	    		putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
-    	}
-    	putchar('\n');
-    	return (0);
+	}
+
+	putchar('\n');
+	return (0);
 }
 
