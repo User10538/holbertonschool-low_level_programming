0cf3f824d47c7576c0fc244f51ebd1da37c73957
User10538 <10538@holbertonstudents.com>
2025-02-18 04:31:26 +0000

    remove spacing

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Return - Always 0 (success)
+ * main - Entry point
+ * Description: Prints the alphabet in lowercase except 'e' and 'q'.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch;
 
-	for(ch = 'a'; ch <= 'z'; ch++)
-		if (ch != 'e' && ch != 'q'){
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
 			putchar(ch);
-		}
+	}
 	putchar('\n');
-	return(0);
 
+	return (0);
 }
+
