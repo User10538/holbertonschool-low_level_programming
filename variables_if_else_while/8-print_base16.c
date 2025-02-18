095aacf200957d69538f35b2179ff14dca6619be
User10538 <10538@holbertonstudents.com>
2025-02-18 12:47:18 +0000

    Fix to Betty

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Prints the lowercase alphabet in reverse
+ * main - Prints all numbers of base 16 in lowercase
  * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
-	int n;
-	for(n = '0'; n <= '9'; n++)
-		putchar(n);
-	
-	for(n = 'a'; n <= 'f'; n++)
-		putchar(n);
-		
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (num = 'a'; num <= 'f'; num++)
+		putchar(num);
+
 	putchar('\n');
-	
-	return(0);
+
+	return (0);
 }
+
