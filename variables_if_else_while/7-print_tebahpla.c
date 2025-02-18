0f5e845e51bdde6dfd4a877a00c076e716dd6307
User10538 <10538@holbertonstudents.com>
2025-02-18 12:27:00 +0000

    Fix Betty coding

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single-digit numbers of base 10 starting from 0
+ * main - Prints the lowercase alphabet in reverse
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
-	
-	for(ch='z'; ch >= 'a'; ch--)
-	
-	putchar(ch);
-	
-	putchar('\n');
-	
-	return(0);
 
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
 }
+
