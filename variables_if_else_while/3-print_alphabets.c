2b9dca9fd4e929d906ddda4c46fb3dca96d48b85
User10538 <10538@holbertonstudents.com>
2025-02-18 04:07:36 +0000

    Fix errors

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch='a';
+
+	while(ch <= 'z')
+		putchar(ch++);
+	ch='A';
+	
+	while(ch <= "Z')
+
+	
+	putchar('\n');
+
+
+	return(0);
+}
