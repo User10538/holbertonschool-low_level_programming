657d2e3c90959c4c34b9ade16653c93f30e10b45
User10538 <10538@holbertonstudents.com>
2025-02-19 01:42:10 +0000

    Fix Betty coding style

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char text[] = "_putchar\n";
-    	int i;
-    
-	for (i = 0; text[i] != '\0'; i++)
-    	{
-		_putchar(text[i]);
-    	}
-    
-	return (0);
+    char text[] = "_putchar\n";
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        _putchar(text[i]);
+    }
+
+    return (0);
 }
 
