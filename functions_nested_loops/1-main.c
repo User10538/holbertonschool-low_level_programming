0c1168645cccbc5efab534ec5423a8a6409ed4bc
User10538 <10538@holbertonstudents.com>
2025-02-19 05:00:07 +0000

    Test

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();  /** Call the print_alphabet function*/
+    return (0);  /** Return 0 to indicate successful execution*/
+}
+
