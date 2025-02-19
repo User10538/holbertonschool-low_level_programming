5a06731c5aca8dfbfa2159f3e942e4f77a5a20a4
User10538 <10538@holbertonstudents.com>
2025-02-19 12:23:20 +0000

    Fix Betty error

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,26 +6,27 @@
 void times_table(void)
 {
 	int row, col, prod;
-    
+
 	for (row = 0; row <= 9; row++)
-    	{
+	{
 		for (col = 0; col <= 9; col++)
 		{
-	    		prod = row * col;
-	    		if (col == 0)
-				_putchar('0' + prod); 
-	    		else
-	    		{
+			prod = row * col;
+
+			if (col == 0)
+				_putchar('0' + prod);
+			else
+			{
 				_putchar(',');
 				_putchar(' ');
 				if (prod < 10)
-		    			_putchar(' '); 
-				_putchar('0' + (prod / 10));
-		
+					_putchar(' ');
+				_putchar('0' + (prod / 10)); /* Tens place */
 				if (prod >= 10)
-		    			_putchar('0' + (prod % 10));
-	    		}
+					_putchar('0' + (prod % 10)); /* Ones place */
+			}
 		}
 		_putchar('\n');
-    	}
+	}
 }
+
