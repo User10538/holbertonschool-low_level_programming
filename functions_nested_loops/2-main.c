095693e8391df32825c1463b44a749d9de873921
User10538 <10538@holbertonstudents.com>
2025-02-19 07:28:48 +0000

    Print alphabet

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	print_alphabet();  /** Task 1*/
 	
+	print_alphabet_x10(); /**Task 2*/
     	return (0);  /** Return 0 to indicate successful execution*/
 }
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_alphabet_x10(void);/**this is for task 2*/
 void print_alphabet(void);/**this is for task 1*/
 int _putchar(char c); /**this is for task 0*/
 
