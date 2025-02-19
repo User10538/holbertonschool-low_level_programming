f63e9235ed9deddba3616c13f219fb99be209375
User10538 <10538@holbertonstudents.com>
2025-02-19 10:02:02 +0000

    Fix Betty error

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,7 @@ int print_last_digit(int n)
 	int find_last_digit = n % 10;
 	if (find_last_digit < 0)
 		find_last_digit = -find_last_digit;
+
 	_putchar(find_last_digit + '0');
-	return find_last_digit;
+	return (find_last_digit);
 }
