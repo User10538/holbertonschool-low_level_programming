2cbc944fd1d26bea7f9adbd00c87f8e3229b1892
User10538 <10538@holbertonstudents.com>
2025-02-19 08:21:08 +0000

    Test push

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks if a character is lowercase
+ * @c: the character to check
+ * Description: This function checks if the given character
+ * is a lowercase letter from 'a' to 'z'.
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _isalpha(int c);/**this is for task 4*/
 int _islower(int c);/**this is for task 3*/
 void print_alphabet_x10(void);/**this is for task 2*/
 void print_alphabet(void);/**this is for task 1*/
