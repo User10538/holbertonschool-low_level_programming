21458d6309c3aedb05052f528e21d9d7f5cb3f1e
User10538 <10538@holbertonstudents.com>
2025-02-19 07:50:19 +0000

    Fix Betty error

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase,
  * followed by a new line each time.
+ *  * _islower - checks if a character is lowercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 is capital
  */
 int _islower(int c)
 {
- /** _islower - checks if a character is lowercase
- * @c: the character to check
- *
- * Return: 1 if c is lowercase, 0 otherwise*/
 	return (c >= 'a' && c <= 'z');
 }
 
