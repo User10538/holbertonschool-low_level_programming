bc92b1152eb5618dfbb0a4f7cea4d0daf5c9e071
User10538 <10538@holbertonstudents.com>
2025-02-19 07:44:00 +0000

    using islower function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase,
+ * followed by a new line each time.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
