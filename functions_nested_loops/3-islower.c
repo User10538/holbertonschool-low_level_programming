12d1e83dba28febe89fde503d33d31dff5133fdd
User10538 <10538@holbertonstudents.com>
2025-02-19 07:57:46 +0000

    Fix Betty error

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,10 +3,8 @@
 /**
  * _islower - checks if a character is lowercase
  * @c: the character to check
- *
- * Description: This function checks if the given character 
- * is a lowercase letter from 'a' to 'z'. 
- *
+ * Description: This function checks if the given character
+ * is a lowercase letter from 'a' to 'z'.
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
