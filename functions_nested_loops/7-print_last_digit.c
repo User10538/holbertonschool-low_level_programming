847800378f699177037280aa7baf2c372cd7018b
User10538 <10538@holbertonstudents.com>
2025-02-19 10:08:04 +0000

    Fix Betty error

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _putchar - Returns the value of the last digit
+ * print_last_digit - Returns the value of the last digit.
  * @n: The integer whose last digit will be printed.
+ *
+ * Return: The last digit of the number.
  */
 
 
 int print_last_digit(int n)
 {
 	int find_last_digit = n % 10;
+
 	if (find_last_digit < 0)
 		find_last_digit = -find_last_digit;
 
