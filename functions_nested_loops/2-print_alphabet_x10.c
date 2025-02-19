34f5f129d5318ddf57b71948c948fa07a1d35906
User10538 <10538@holbertonstudents.com>
2025-02-19 08:11:13 +0000

    Fix Betty error

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * Description: function that prints 10 times the alphabet, 
- * in lowercase,
- * followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+ *
+ * Description: This function prints the alphabet in lowercase, from 'a' to 'z',
+ *              a total of 10 times, each time followed by a new line.
+ *
  * Return: Nothing (void).
  */
-
 void print_alphabet_x10(void)
 {
 	char ch;
