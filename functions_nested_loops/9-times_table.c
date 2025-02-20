e49added5bb3d0cd013104f706dac760ecec61af
User10538 <10538@holbertonstudents.com>
2025-02-20 05:53:30 +0000

    Fix time tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,32 +6,41 @@
  * Description: This function prints a multiplication table
  * for the number 9, formatted with appropriate spacing.
  */
+
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++)
 
-            if (j == 0)
-                _putchar('0' + product);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+	{
+		for (j = 0; j <= 9; j++)
 
-                if (product < 10)
-                    _putchar(' '); /* Add space for single digits */
-                
-                _putchar('0' + (product / 10));
-                if (product >= 10)
-                    _putchar('0' + (product % 10));
-            }
-        }
-        _putchar('\n');
-    }
+		{
+			product = i * j;
+
+			if (j == 0)
+				
+				_putchar(product + '0'); /* Print first value without comma */
+
+			else
+
+	    		{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+		    			_putchar(' '); /* Add extra space for alignment */
+				
+		 		if (product < 10)
+		    			_putchar(product + '0');
+				else
+				{
+		    			_putchar((product / 10) + '0'); /* Print tens */
+		    			_putchar((product % 10) + '0'); /* Print ones */
+		 		}
+	    		}
+		}
+		_putchar('\n');
+    	}
 }
 
