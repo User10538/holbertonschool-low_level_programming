3c1d8eebdf40d0c25ca62f380cd89ae49b3e17f9
User10538 <10538@holbertonstudents.com>
2025-02-20 06:26:03 +0000

    Create files for task 12

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_times_table(int n);/**this is task 12*/
 void print_to_98(int n);/**this is task 11*/
 int add(int, int);/**this is task 10*/
 void times_table(void);/**this is task 9*/
