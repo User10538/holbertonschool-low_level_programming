aa795599b3f7f2e97c5ed76e7f9375dd69c25c11
User10538 <10538@holbertonstudents.com>
2025-02-22 04:39:58 +0000

    Fix Betty error

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,34 +7,33 @@
  */
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
+			if (j != 0) /* Print comma and spaces after the first number */
+			{	_putchar(',');
+				_putchar(' ');
 
-            if (j != 0) /* Print comma and spaces after the first number */
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10)
-                    _putchar(' '), _putchar(' ');
-                else if (product < 100)
-                    _putchar(' ');
-            }
+				if (product < 10)
+					_putchar(' '), _putchar(' ');
+				else if (product < 100)
+					_putchar(' ');
+			}
+			if (product >= 100) /* Print hundreds */
+				_putchar((product / 100) + '0');
+			if (product >= 10) /* Print tens */
+				_putchar(((product / 10) % 10) + '0');
+			_putchar((product % 10) + '0'); /* Print ones */
+		}
 
-            if (product >= 100) /* Print hundreds */
-                _putchar((product / 100) + '0');
-            if (product >= 10) /* Print tens */
-                _putchar(((product / 10) % 10) + '0');
-            _putchar((product % 10) + '0'); /* Print ones */
-        }
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
 
