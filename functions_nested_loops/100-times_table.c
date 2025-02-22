0d627f4e730fe774ef2e12f49122ce66064e9abd
User10538 <10538@holbertonstudents.com>
2025-02-22 04:05:26 +0000

    100 times

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_times_table(int n)
+{
+	int i, j;
+
+    if (n > 15 || n < 0)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            if (j > 0)
+                printf(", ");
+
+            printf("%d", i * j);
+        }
+        printf("\n");
+    }
+}
+
