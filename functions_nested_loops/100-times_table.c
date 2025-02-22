cf5a1a81481b985c521f3c02d64239a707cb5ed4
User10538 <10538@holbertonstudents.com>
2025-02-22 04:15:02 +0000

    using puchar instead of printf

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,23 +1,40 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The multiplication table to print.
+ */
 void print_times_table(int n)
 {
-	int i, j;
+    int i, j, product;
 
-    if (n > 15 || n < 0)
+    if (n < 0 || n > 15)
         return;
 
     for (i = 0; i <= n; i++)
     {
         for (j = 0; j <= n; j++)
         {
-            if (j > 0)
-                printf(", ");
+            product = i * j;
 
-            printf("%d", i * j);
+            if (j != 0) /* Print comma and spaces after the first number */
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (product < 10)
+                    _putchar(' '), _putchar(' ');
+                else if (product < 100)
+                    _putchar(' ');
+            }
+
+            if (product >= 100) /* Print hundreds */
+                _putchar((product / 100) + '0');
+            if (product >= 10) /* Print tens */
+                _putchar(((product / 10) % 10) + '0');
+            _putchar((product % 10) + '0'); /* Print ones */
         }
-        printf("\n");
+        _putchar('\n');
     }
 }
 
