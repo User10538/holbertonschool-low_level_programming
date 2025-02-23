fad59540e4ec690b9fcbb31339f8b0b5f2e1be90
User10538 <10538@holbertonstudents.com>
2025-02-23 23:24:36 +0000

    Fix Betty error

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
- * main- entry point
- *
  * _isupper - Checks if a character is uppercase
- * @c: The character to check
+ * @c: The character to be checked
+ *
+ * Description: This function takes an integer, which represents
+ * a character in ASCII. If the character is an uppercase letter
+ * (A-Z), the function returns 1. Otherwise, it returns 0.
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 
 int _isupper(int c)
 {
