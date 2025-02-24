9ce91c815cf66d6b8728a0dfef9aef77438a396f
User10538 <10538@holbertonstudents.com>
2025-02-24 04:42:53 +0000

    using printf

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int n;
+
+	for (n ='1'; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		
+			printf("FizzBuzz");
+		
+		else if (n % 3 == 0)
+		
+			printf("Fuzz");
+
+		
+		else if (n % 5 == 0)
+		
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n < 100)
+			printf(" ");
+		
+		
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
