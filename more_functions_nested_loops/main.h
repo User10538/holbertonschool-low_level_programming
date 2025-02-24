b03bb89bb99041f1447865d4eca089ed777c0ea3
User10538 <10538@holbertonstudents.com>
2025-02-24 02:57:30 +0000

    Fix Betty Error

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_line(int n);/**this is for task 6*/
 void more_numbers(void);/**this is for task 5*/
 void print_most_numbers(void);/**this is for task 4*/
 void print_numbers(void);/**this is for task 3*/
