08ad1c5e6eb0ded56672d9d84011dd7f0a8194fc
User10538 <10538@holbertonstudents.com>
2025-02-24 23:51:09 +0000

    len of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+	return strlen(s);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _strlen(char *s);/**this is for Task2*/
 void swap_int(int *a, int *b);/**this is for task 1*/
 void reset_to_98(int *n);/**thi is for task 0*/
 int _putchar(char c); /**this is for putchar*/
