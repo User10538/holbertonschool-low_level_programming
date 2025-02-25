2dfbc1027155b9214a36ea1ec21a3cb0a109fad3
User10538 <10538@holbertonstudents.com>
2025-02-25 10:08:54 +0000

    Fix Betty error

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-    char *tothe_end = s;
+	char *tothe_end = s;
 
-    while (*tothe_end != '\0')
-        tothe_end++;
+	while (*tothe_end != '\0')
+		tothe_end++;
 
-    while (tothe_end > s)
-    {
-        tothe_end--;
-        _putchar(*tothe_end);
-    }
+	while (tothe_end > s)
+	{
+		tothe_end--;
+		_putchar(*tothe_end);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
