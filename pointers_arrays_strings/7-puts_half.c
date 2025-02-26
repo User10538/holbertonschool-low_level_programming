17d0f19bd75c3d7356b182d3ed1a3ee3356e53c1
User10538 <10538@holbertonstudents.com>
2025-02-26 03:15:46 +0000

    Print second half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+
+void puts_half(char *str)
+{
+	/** find the len of the string*/
+
+	char *ptr = str;
+	int len = 0;
+	char *start;
+
+	while (*ptr)
+	{
+		ptr++;
+		len++;
+	}
+
+	if (len % 2 == 1)/**modulus operator (%) divides one number by another and returns the remainder of that division.*/
+	{
+		len = (len +1) /2;
+	}
+	else
+	{
+		len = len/2;
+	}
+
+	start = str + len; /**point to the start pf the 2nd part*/
+
+	while (*start)
+	{
+		putchar(*start);
+		start++;
+	}
+
+
+
+	_putchar('\n');
+
+
+}
