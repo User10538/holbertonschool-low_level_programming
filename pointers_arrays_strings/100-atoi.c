ffd194dc6b32588cc4a93500dd379e9621b482c7
User10538 <10538@holbertonstudents.com>
2025-02-26 06:31:26 +0000

    Fix error for mixed symbols

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 #include <limits.h>
+
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Return: the integer value of the string
+ * Return: The integer value of the string
  */
 int _atoi(char *s)
 {
-	int signs = 1, new_int = 0;
+    int signs = 1, new_int = 0, found_num = 0;
 
-	if (*s == '-')
-	{
-		/**printf("%c", *s);*/
-		signs = -signs ;
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-        if (new_int > (INT_MAX / 10) || (new_int == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
-        {
-            /* Handle overflow */
-            return (signs == 1) ? INT_MAX : INT_MIN;
-        }
-        new_int = new_int * 10 + (*s - '0');
-        s++;
+    /* Skip non-numeric characters and count thei signs */
+    while (*s && (*s < '0' || *s > '9'))
+    {
+        if (*s == '-')
+		/**printf(%c, *s);*/
+		signs = -signs;
+        s++; /* Move to the next character */
     }
 
-	return (signs * new_int);
+    /* Convert number while max and min */
+    while (*s >= '0' && *s <= '9')
+    {
+        if (new_int > (INT_MAX / 10) || (new_int == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+            return (signs == 1) ? INT_MAX : INT_MIN;
 
+        new_int = new_int * 10 + (*s - '0');
+        s++;
+        found_num = 1;
+    }
+
+    return (found_num ? signs * new_int : 0); /**shorthand for an if-else statement*/
 }
+
