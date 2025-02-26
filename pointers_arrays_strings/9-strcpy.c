e17fd2a7148eed0697b719a046b270649bb75068
User10538 <10538@holbertonstudents.com>
2025-02-26 05:02:48 +0000

    Fix error by using while loop

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy (dest, src);
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
