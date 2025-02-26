7ecaeca6df8054a9ad916b97252cb4b356014954
User10538 <10538@holbertonstudents.com>
2025-02-26 12:04:57 +0000

    Fix Betty error

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,22 +8,24 @@
  */
 char *leet(char *s)
 {
-    int i = 0, j;
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
+	int i = 0, j = 0;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-    while (s[i] != '\0')  /* First loop to traverse the input string */
-    {
-        j = 0;
-        while (letters[j] != '\0' && s[i] != letters[j]) /* Second loop to check replacements */
-            j++;
+	while (s[i] != '\0')  /* First loop to traverse the input string */
+	{
+		while (letters[j] != '\0' && s[i] != letters[j])
+			/* Second loop check replacements */
+			j++;
 
-        if (letters[j] != '\0') /* If a match was found, replace it */
-            s[i] = numbers[j];
+		if (letters[j] != '\0') /* If a match was found, replace it */
+		{
+			s[i] = numbers[j];
+		}
 
-        i++;
-    }
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
 
