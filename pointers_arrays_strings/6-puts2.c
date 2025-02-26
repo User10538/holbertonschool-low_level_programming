39f3b35524806c018977eaadda484d8e28c600bb
User10538 <10538@holbertonstudents.com>
2025-02-26 01:33:41 +0000

    Add a checker to see if here is another char before moving on

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The input string.
+ */
+
 void puts2(char *str)
 {
-	
+
 	while (*str != '\0')
 	{
 		_putchar(*str);
 
 		str++;
+
+		if (*str != '\0')
+
+			str++;
 	}
 
 	_putchar('\n');
