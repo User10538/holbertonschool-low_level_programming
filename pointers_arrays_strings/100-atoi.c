e315290947d0c7c844de91f8284075e126a0ef36
User10538 <10538@holbertonstudents.com>
2025-02-26 06:38:13 +0000

    Fix Betty error

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,30 +8,34 @@
  *
  * Return: The integer value of the string
  */
+
 int _atoi(char *s)
 {
-    int signs = 1, new_int = 0, found_num = 0;
+	int signs = 1, new_int = 0, found_num = 0;
 
-    /* Skip non-numeric characters and count thei signs */
-    while (*s && (*s < '0' || *s > '9'))
-    {
-        if (*s == '-')
-		/**printf(%c, *s);*/
-		signs = -signs;
-        s++; /* Move to the next character */
-    }
 
-    /* Convert number while max and min */
-    while (*s >= '0' && *s <= '9')
-    {
-        if (new_int > (INT_MAX / 10) || (new_int == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
-            return (signs == 1) ? INT_MAX : INT_MIN;
+	/* Skip non-numeric characters and count thei signs */
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+			/**printf(%c, *s);*/
+			signs = -signs;
+		s++; /* Move to the next character */
+	}
 
-        new_int = new_int * 10 + (*s - '0');
-        s++;
-        found_num = 1;
-    }
+	/* Convert number while max and min */
+	while (*s >= '0' && *s <= '9')
+	{
+		if (new_int > (INT_MAX / 10) || (new_int == (INT_MAX / 10)
+		&& (*s - '0') > (INT_MAX % 10)))
+			return ((signs == 1) ? INT_MAX : INT_MIN);
 
-    return (found_num ? signs * new_int : 0); /**shorthand for an if-else statement*/
+		new_int = new_int * 10 + (*s - '0');
+		s++;
+		found_num = 1;
+	}
+
+	return (found_num ? signs * new_int : 0);
+	/**shorthand for an if-else statement*/
 }
 
