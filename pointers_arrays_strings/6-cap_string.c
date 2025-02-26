ab279bc65d3ee73927a7dff812153a7fafb54b37
User10538 <10538@holbertonstudents.com>
2025-02-26 11:17:48 +0000

    Capitalise all words

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,5 +3,25 @@
 
 char *cap_string(char *s)
 {
-	
+	int i = 0, j = 0, capitalise = 1;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (s[i] != '\0')
+	{
+		if(s[i] >= 'a' && s[i] <= 'z' && capitalise)
+		{
+		
+			s[i]-= 32;
+		}
+
+		capitalise = 0;
+
+		if (s[i] == separators[j])
+		{
+			capitalise = 1;
+
+		}
+	}
+
+	return (s);
 }
