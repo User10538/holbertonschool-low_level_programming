37bd355aa3061b15582b72161335799d60a39c09
User10538 <10538@holbertonstudents.com>
2025-02-26 01:35:41 +0000

    Upload files to save

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void puts_half(char *str);/**task 7*/
 void puts2(char *str);/**task 6*/
 void rev_string(char *s);/**this is for Task 5*/
 void print_rev(char *s);/**this is for Task 4*/
