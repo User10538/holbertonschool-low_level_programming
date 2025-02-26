d22956cbbc6874358006d96703d2d2db336e3429
User10538 <10538@holbertonstudents.com>
2025-02-26 06:15:07 +0000

    Fix errors

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
@@ -10,22 +11,23 @@ int _atoi(char *s)
 {
 	int signs = 1, new_int = 0;
 
-	while (*s && ( *s == ' ' || *s == '+' || *s == '-'))
+	if (*s == '-')
 	{
-		if (*s == '-')
-		{
-			/**printf("%c", *s);*/
-			signs = -signs ;
-			s++;
-		}
+		/**printf("%c", *s);*/
+		signs = -signs ;
+		s++;
 	}
 
 	while (*s >= '0' && *s <= '9')
 	{
-		new_int = new_int * 10 + (*s - '0');
-		s++;
-	}
-
+        if (new_int > (INT_MAX / 10) || (new_int == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+        {
+            /* Handle overflow */
+            return (signs == 1) ? INT_MAX : INT_MIN;
+        }
+        new_int = new_int * 10 + (*s - '0');
+        s++;
+    }
 
 	return (signs * new_int);
 
