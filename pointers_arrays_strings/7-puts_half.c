c37ab32c9bfdf61ad16e73000238874f0bde81f0
User10538 <10538@holbertonstudents.com>
2025-02-26 03:51:41 +0000

    Fix Betty error

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * puts_half - print the second half of the string
+ * @str: the input string
+ *
+ * The function calculates the length of the string and prints the second half.
+ * If the length is odd, it starts from the middle character; otherwise, it
+ * starts from the character just after the middle.
+ */
 
 void puts_half(char *str)
 {
-	/** find the len of the string*/
 
-	char *ptr = str;
+
+	char *ptr = str, *start;
 	int len = 0;
-	char *start;
 
 	while (*ptr)
 	{
@@ -16,13 +22,13 @@ void puts_half(char *str)
 		len++;
 	}
 
-	if (len % 2 == 1)/**modulus operator (%) divides one number by another and returns the remainder of that division.*/
+	if (len % 2 == 1)
 	{
-		len = (len +1) /2;
+		len = (len + 1) / 2;
 	}
 	else
 	{
-		len = len/2;
+		len = len / 2;
 	}
 
 	start = str + len; /**point to the start pf the 2nd part*/
