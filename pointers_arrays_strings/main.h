dc74d486769ed748300cd99b25a15333db630e94
User10538 <10538@holbertonstudents.com>
2025-02-26 08:40:30 +0000

    Reverse the content of array using temp

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void reverse_array(int *a, int n);/**task 4*/
 int _strcmp(char *s1, char *s2);/**task 3*/
 char *_strncpy(char *dest, char *src, int n);/**task 2*/
 char *_strncat(char *dest, char *src, int n);/**task 1*/
