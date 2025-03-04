e5c851517f74f2a7b27be3e763d7ee420ae9a28a
User10538 <10538@holbertonstudents.com>
2025-03-04 00:01:21 +0000

    add a 0 to the unsigned int

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memset() function fills the first n bytes
- *  of the memory area pointed to by s
- *   with the constant byte b
- *   @s:array
- *   @n: is an int
- *   @b: is a char
- *   Return: pointer to memory area s
+ * Description - fills memory with a constant byte.
+ *
+ * _memset - fills memory with a constant byte
+ *
+ * @s:array
+ * @n: is an int
+ * @b: is a char
+ * Return: pointer to memory area s
  */
 
 
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
