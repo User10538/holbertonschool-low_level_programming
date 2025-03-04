d97e5adfac4bb9fc6bd520089228da9bf583b07e
User10538 <10538@holbertonstudents.com>
2025-03-04 14:11:39 +0000

    Fix error

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,36 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: The main string.
- * @accept: The characters to search for.
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The main string to search.
+ * @accept: The characters to match.
  *
- * Return: byte in s match bytes in accept,
- * or NULL if no such byte is found
- *
+ * Return: A pointer to the first occurrence in s of any byte from accept,
+ * or NULL if no match is found.
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i = 0, j;
+    int i = 0, j;
 
-	while (s[i] != '\0')
-	{
-		j = 0;
+    while (s[i] != '\0') /* Loop through s */
+    {
+        j = 0;
+        while (accept[j] != '\0') /* Loop through accept */
+        {
+            if (s[i] == accept[j]) /* If match found */
+                return (s + i); /* Return pointer to match */
+            j++;
+        }
+        i++;
+    }
 
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				return (s + i);/* Return pointer to match */
-				j++;
-			}
-			i++;
-		}
-	}
-
-	return (NULL);
-
+    return (NULL); /* No match found */
 }
+
