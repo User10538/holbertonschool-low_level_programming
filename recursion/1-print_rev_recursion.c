6317245e17070af5b3c99644cb0eec6e75fa0cba
User10538 <10538@holbertonstudents.com>
2025-03-05 06:50:04 +0000

    Reverse string

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,10 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
+	if (*s == '\0')
+	return;
 
-    _print_rev_recursion(s + 1); /** Recursive call with next character*/
-    _putchar(*s);
+	_print_rev_recursion(s + 1); /** Recursive call with next character*/
+	_putchar(*s);
 }
 
