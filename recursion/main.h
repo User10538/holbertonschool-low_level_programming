e9a3b65161d81687b043c048f27c75dd955fdd3c
User10538 <10538@holbertonstudents.com>
2025-03-05 06:42:28 +0000

    Reverse string

--- recursion/main.h
+++ recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN
 
-
+void _print_rev_recursion(char *s);/**task 1*/
 void _puts_recursion(char *s);/**this is for task 0*/
 int _putchar(char c); /**this is for putchar*/
 
