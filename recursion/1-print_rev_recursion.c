fea3a775babc84fe6e689b8ceb69fe9b19ed99b8
User10538 <10538@holbertonstudents.com>
2025-03-05 06:47:56 +0000

    Reverse string

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts_recursion - Prints a string followed by a new line using recursion.
- * @s: The string to print.
+ * _print_rev_recursion - Prints a string in reverse using recursion.
+ * @s: The string to print in reverse.
  */
 void _print_rev_recursion(char *s)
 {
     if (*s == '\0')
-    {
-        _putchar('\n');
         return;
-    }
 
+    _print_rev_recursion(s + 1); /** Recursive call with next character*/
     _putchar(*s);
-    _puts_recursion(s - 1);
 }
+
