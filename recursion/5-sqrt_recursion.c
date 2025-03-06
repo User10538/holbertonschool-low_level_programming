d4a72c2d5cb947b7d55b655432e70797b9b3b87a
User10538 <10538@holbertonstudents.com>
2025-03-06 13:33:44 +0000

    sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,11 +4,27 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  *
+ * _sqrt_recursion_helper: Helper function to find square root recursively
+ *
  * @n : integer.
+ * @i: the integer
  *
  * Return: sqauare root of a number
  *
  */
+int _sqrt_recursion_helper(int n, int i)
+{
+	if (i * i > n)  /** If i^2 exceeds n, no natural sqrt exists*/
+	return (-1);
+
+
+	if (i * i == n) /** Found the sqrt*/
+	return (i);
+
+	return (_sqrt_recursion_helper(n, i + 1)); /** Check next number*/
+
+
+}
 
 int _sqrt_recursion(int n)
 {
@@ -17,19 +33,4 @@ int _sqrt_recursion(int n)
 	return (-1);
 
 	return (_sqrt_recursion_helper(n, 1));
-}
-
-int _sqrt_recursion_helper(int n, int i)
-{
-	if (i * i > n)  /** If i^2 exceeds n, no natural sqrt exists*/
-		
-		return (-1);
-	
-	if (i * i == n) /** Found the sqrt*/
-		
-		return (i);
-	
-	return (_sqrt_recursion_helper(n, i + 1)); /** Check next number*/
-
-
 }
