f35a536cd7f335bf971993fa6099c0cd6ab09224
User10538 <10538@holbertonstudents.com>
2025-03-07 14:22:18 +0000

    Fix Betty error

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 	int count;
 
 	/**printf("This program was called with \"%s\".\n", argv[0]);*/
-	
+
 	for (count = 0; count < argc; count++)
 	{
 		/**printf("argv[%d] = %s\n", count, argv[count]);*/
 		printf("%s\n", argv[count]);
 	}
-	
+
 	return (0);
 }
