4d9266d251fb609e4e12eb22c6c3ccb3959478dd
User10538 <10538@holbertonstudents.com>
2025-03-07 14:50:59 +0000

    Fix Betty error

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,28 +4,28 @@
 /**
  * main - print the result of the multiplication
  * @argc: number of arguments
- * @argv: array of arguments 
+ * @argv: array of arguments
  *
  * Return: 0 (success),return 1 if error
  */
 int main(int argc, char *argv[])
 {
-        int num1, num2, multiple;
-		
+	int num1, num2, multiple;
+
 	/* Check if exactly two arguments are provided */
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	/* Convert arguments to integers */
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	
+
 	multiple = num1 * num2;
 	printf("%d\n", multiple);
 
 	return (0);
-	
+
 }
