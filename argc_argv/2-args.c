09305fb70a0fc5038bbd4ee12788727caff02b50
User10538 <10538@holbertonstudents.com>
2025-03-07 14:21:30 +0000

    Fix error

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -12,22 +12,12 @@ int main(int argc, char *argv[])
 	int count;
 
 	/**printf("This program was called with \"%s\".\n", argv[0]);*/
-
-	if (argc > 1)
-
+	
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 1; count < argc; count++)
-
-		{
-			/**printf("argv[%d] = %s\n", count, argv[count]);*/
-			printf("%s\n", argv[count]);
-		}
+		/**printf("argv[%d] = %s\n", count, argv[count]);*/
+		printf("%s\n", argv[count]);
 	}
-
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
-
+	
 	return (0);
 }
