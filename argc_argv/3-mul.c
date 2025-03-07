72486ae5e91aa163e77475bf41ade491f4fdf91d
User10538 <10538@holbertonstudents.com>
2025-03-07 14:45:05 +0000

    Multiple two int

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the result of the multiplication
+ * @argc: number of arguments
+ * @argv: array of arguments 
+ *
+ * Return: 0 (success),return 1 if error
+ */
+int main(int argc, char *argv[])
+{
+        int num1, num2, multiple;
+	
+	/* Convert arguments to integers */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	
+	multiple = num1 * num2;
+	printf("%d\n", multiple);
+
+	return (0);
+	
+	/* Check if exactly two arguments are provided */
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+        return (0);
+}
