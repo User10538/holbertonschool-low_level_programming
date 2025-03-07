a467d8bc98d99c56a00ecf22c69c50987e8b9be2
User10538 <10538@holbertonstudents.com>
2025-03-07 15:06:48 +0000

    Fix error

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,32 +1,31 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - adds positive numbers.
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (success),return 1 if error
+ * Return: 0 (success), 1 (if error or non-positive number)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, addition;
+	int num, sum = 0;
 
-	/* Check if exactly two arguments are provided */
-
-	if (argc != 3)
+	for (int i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		num = atoi(argv[i]);
+
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += num;
 	}
 
-	/* Convert arguments to integers */
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	addition = num1 + num2;
-
-	printf("%d\n", addition);
+	printf("%d\n", sum);
 	return (0);
-
 }
+
