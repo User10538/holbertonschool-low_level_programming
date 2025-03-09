a2bda2c19b7f1faf4bf7dca6e58d824dfea5b1f7
User10538 <10538@holbertonstudents.com>
2025-03-09 11:23:03 +0000

    Fix error

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,32 +1,30 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * _strchr -  locate char in string
+ * _strchr - locate char in string
+ * @s: pointer to the string to be searched
  * @c: char to be located
- * @s:pointer to the string to be searched
- * @null
  *
  * Return: Pointer to the first occurrence of c, or NULL if not found
  */
-
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
+    while (s[i] != '\0')
+    {
+        if (s[i] == c)
+        {
+            return (&s[i]); /** Return pointer to the first occurrence of c*/
+        }
+        i++;
+    }
 
-		if (s[i] == c)
-		{
-			return (&s[i]);
-			i++;
-		}
-	if (c == '\0')
-	{
-		return (&s[i]);
-	}
+    if (c == '\0')
+    {
+        return (&s[i]);
+    }
 
-	return (NULL);
-
+    return (NULL);
 }
-
