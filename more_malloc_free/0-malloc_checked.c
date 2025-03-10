ac03d128a79bea31e2ac095a2728edbd34a6cc05
User10538 <10538@holbertonstudents.com>
2025-03-10 15:47:36 +0000

    Upload files to save

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: first string
+ * @height: second string
+ *
+ * Return: NULL or on failure or contains the contents of s1 and s2.
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+
+	char * arr;
+	int size = 0;
+
+	arr = malloc(size + 1);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN
+
+void *malloc_checked(unsigned int b);/** task 0*/
+int _putchar(char c); /**this is for putchar*/
+
+#endif /* MAIN_H */
