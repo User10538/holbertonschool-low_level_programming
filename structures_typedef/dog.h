2d50a46d866a0959a0e571b895c4a886e7cde132
User10538 <10538@holbertonstudents.com>
2025-03-11 04:24:43 +0000

    Fix Betty error

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,6 +18,15 @@ struct dog
 
 };
 
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);/**task 1*/
 
 #endif /* DOG_H */
