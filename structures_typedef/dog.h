d80f1209683c997eeed146350b3d4634aadb0120
User10538 <10538@holbertonstudents.com>
2025-03-11 04:28:55 +0000

    Fix Betty error

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,31 +2,29 @@
 #define DOG_H
 
 /**
- * struct dog - Define a new type
- *
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
+ * struct dog - Define a new type for dog details
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
  */
-
 struct dog
 {
-
-	char *name;
-	float age;
-	char *owner;
-
+    char *name;
+    float age;
+    char *owner;
 };
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: struct dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - Initializes a struct dog variable
+ * @d: Pointer to struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
+ * Description: This function initializes a variable of type struct dog
+ *              with the given name, age, and owner.
  */
-
-void init_dog(struct dog *d, char *name, float age, char *owner);/**task 1*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
+
