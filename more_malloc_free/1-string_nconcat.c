17db299545e0e2bd24f9fac170ed9c0ff2a9a8a0
User10538 <10538@holbertonstudents.com>
2025-03-11 00:45:43 +0000

    concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,6 +8,8 @@
  * @s1: first string
  * @s2: second string
  *
+ * @n: integer
+ *
  * Return: NULL or on failure or contains the contents of s1 and s2.
  */
 
@@ -15,17 +17,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
 	unsigned int i, j, len1 = 0, len2 = 0;
-        char *new_string;
+	char *new_string;
 
-	        /**if NULL is passed, treat it as an empty string*/
+	/**if NULL is passed, treat it as an empty string*/
 
-        if (s1 != NULL)
-        while (s1[len1])
-                len1++;
+	if (s1 != NULL)
+		while (s1[len1])
+			len1++;
 
-        if (s2 != NULL)
-        while (s2[len2])
-                len2++;
+	if (s2 != NULL)
+		while (s2[len2])
+			len2++;
 
+	/* Use at most `n` bytes from s2 */
+	if (n < len2)
+		len2 = n;
 
+	/* Allocate memory for concatenated string (+1 for null terminator) */
+
+	new_string = malloc(len1 + len2 + 1);
+
+	if (new_string == NULL)
+		return (NULL);
+
+	/* Copy s1 to new_string */
+	for (i = 0; i < len1; i++)
+		new_string[i] = s1[i];
+
+	/* Copy s2 to new_string */
+	for (j = 0; j < len2; j++, i++)
+		new_string[i] = s2[j];
+
+	/* Null-terminate the new string */
+	new_string[i] = '\0';
+
+	return (new_string);
 }
