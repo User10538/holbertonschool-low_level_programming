452ba19e8f4f685bba32d9fd5b27b43bc2a62efd
User10538 <10538@holbertonstudents.com>
2025-03-17 01:20:38 +0000

    To print

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed.
+ * @f: function pointer.
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *)); /** task 0*/
+
+/**
+ * _putchar - to print
+ * c: the character
+ */
+int _putchar(char c); /**this is for putchar*/
+
+#endif /* FUNCATION_POINTERS_H */
+
