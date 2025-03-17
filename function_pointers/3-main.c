47af4ea5f205997472013bf4a3c33252f9901b7c
User10538 <10538@holbertonstudents.com>
2025-03-17 05:56:58 +0000

    program that performs simple operation

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,48 @@
-int main(void)
+#include "3-calc.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point, performs simple operations
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: 0 on success, exit with status 98, 99, or 100 on error
+ */
+int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+	int (*operator)(int, int); /* Declare function pointer */
+	
+	/* Must have exactly 3 arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	/* Get function pointer */
+    	/**operator = 3-get_op_func(argv[2]); */
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0) /* Division by zero */
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	
+	result = operator(num1, num2);
+	printf("%d\n", result);
+	
+	return (0);
 
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -12,7 +12,7 @@
  */
 int op_add(int a, int b)
 {
-	
+	return (a + b);
 }
 
 /**
@@ -22,7 +22,11 @@ int op_add(int a, int b)
  * 
  * Return difference of a and b
  */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
+{
+	return (a - b);
+
+}
 
 /**
  * op_mul -  product of a and b
@@ -31,7 +35,10 @@ int op_sub(int a, int b);
  * 
  * Return product of a and b
  */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
 /**
  * op_div -  division of a by b
@@ -40,7 +47,11 @@ int op_mul(int a, int b);
  * 
  * Return division of a by b
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
+{
+	return (a / b);
+
+}
 
 /**
  * op_mod -  remainder of the division of a by b
@@ -49,4 +60,7 @@ int op_div(int a, int b);
  * 
  * Return remainder of the division of a by b
  */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
+{
+	return ( a % b);
+}
