2ceb8f18742d6d96051e45f9797742182a28560f
User10538 <10538@holbertonstudents.com>
2025-03-31 23:20:11 +0000

    lisy len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * list_len - function that returns the
+ * number of elements in a linked list_t list
+ * @h: const list_t.
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		/*count the node*/
+		count++;
+
+		/*move to the next node*/
+		h = h->next;
+	}
+
+	return (count);
+
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,6 +21,12 @@ typedef struct list_s
 
 } list_t;
 
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: const list_t.
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h);
 
 /**
  * print_list - function that prints all the elements of a list_t list.
