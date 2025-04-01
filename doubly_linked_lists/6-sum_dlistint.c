9e0587959e85119ffdffa03155434c400d44fda1
User10538 <10538@holbertonstudents.com>
2025-04-01 06:12:51 +0000

    Sum of data

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -7,7 +7,17 @@
  * @head: const dlistint_t.
  * Return: sum of nodes
  */
-int sum_dlistint(dlistint_t *head
+int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
 	
-)
+}
