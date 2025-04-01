fe344ba51b9d4bbb9e9db6ec723ad24b99997e82
User10538 <10538@holbertonstudents.com>
2025-04-01 05:01:53 +0000

    Fix betty error

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,14 +4,16 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list.
+ * get_dnodeint_at_index - function that returns the
+ * nth node of a dlistint_t linked list.
  * @head: const dlistint_t.
  * @index: index of node, start at 0
  * Return: number of nodes
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	/*pointer get_nodes used to traverse the list. It starts at the head of the list*/
+	/*pointer get_nodes used to traverse the list.*/
+	/* It starts at the head of the list*/
 	dlistint_t *get_nodes = head;
 	unsigned int count = 0;
 
