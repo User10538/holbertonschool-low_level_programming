451e882ab58476c5520bd63571f9dde94f63dc83
User10538 <10538@holbertonstudents.com>
2025-04-01 06:14:58 +0000

    Fix betty error

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * sum_dlistint - function that returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - function that returns the sum
+ * of all the data (n) of a dlistint_t linked list.
  * @head: const dlistint_t.
  * Return: sum of nodes
  */
@@ -19,5 +20,5 @@ int sum_dlistint(dlistint_t *head)
 	}
 
 	return (sum);
-	
+
 }
