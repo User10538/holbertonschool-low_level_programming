1bce2dc354392630cedd6ca3c53e716205bff7f9
User10538 <10538@holbertonstudents.com>
2025-04-01 02:50:05 +0000

    return number of elements

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -21,6 +21,12 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/**
+ * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ * @h: const dlistint_t.
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h);
 
 /**
  * print_dlistint - function that prints all the elements of a dlistint_t list.
