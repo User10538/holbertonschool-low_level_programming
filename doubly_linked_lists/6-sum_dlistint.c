174de328b2fb564d405ac1938e79c5429e613a17
User10538 <10538@holbertonstudents.com>
2025-04-01 05:18:33 +0000

    Upload file to dave

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of all the data (n) of a dlistint_t linked list.
+ * @head: const dlistint_t.
+ * Return: sum of nodes
+ */
+int sum_dlistint(dlistint_t *head
+{
+	
+)
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -22,6 +22,13 @@ typedef struct dlistint_s
 } dlistint_t;
 
 /**
+ * sum_dlistint - function that returns the sum of all the data (n) of a dlistint_t linked list.
+ * @head: const dlistint_t.
+ * Return: sum of nodes
+ */
+int sum_dlistint(dlistint_t *head);
+
+/**
  * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list.
  * @head: const dlistint_t.
  * @index: index of node, start at 0
