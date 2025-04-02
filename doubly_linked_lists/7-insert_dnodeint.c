28c81b81602eee87b1e09010fadd55f28d635d06
User10538 <10538@holbertonstudents.com>
2025-04-02 10:03:31 +0000

    Upload files to save

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,18 +11,48 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp = *h;
+	dlistint_t *new_nodes, *temp = *h;
 	unsigned int i = 0;
 	
-	if (h == NULL)
+	if (*h == NULL)
 		return (NULL);
 	
-	new_node = malloc(sizeof(dlistint_t));
+	new_nodes = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
+	if (new_nodes== NULL)
 		return (NULL);
+	
+	new_nodes->n = n;
+	
+	/* Insert at the beginning */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	
+	/* Traverse list to find the insertion point */
+       while (temp && i < idx - 1)
+       {
+	        temp = temp->next;
+		i++;
+       }
 
-
-
+       /* free the new nodes*/
+       if (temp == NULL)
+       {
+	       free(new_nodes);
+	       return (NULL);
+       }
+       
+       if (temp->next == NULL)
+	       return (add_dnodeint_end(h, n));
+       
+       /* Inserting in the middle of the list */
+       new_nodes->next = temp->next;
+       new_nodes->prev = temp;
+       
+       if (temp->next)
+	       temp->next->prev = new_nodes;
+       temp->next = new_nodes;
+       
+       return (new_nodes);
 
 }
