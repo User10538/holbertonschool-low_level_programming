baea7f791a60f6bc0a47ce195195eb53d24759d6
User10538 <10538@holbertonstudents.com>
2025-04-02 11:54:33 +0000

    betty error

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,14 +14,14 @@ unsigned int binary_to_uint(const char *b)
 
 	int intNumber = 0, i = 0;
 
-	if (b == NULL|| *b == '\0')
+	if (b == NULL || *b == '\0')
 		return (0);
 
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		
+
 		/*shift the number left by 1 and add new digit*/
 		intNumber = intNumber * 2 + (b[i] - '0');
 		i++;
