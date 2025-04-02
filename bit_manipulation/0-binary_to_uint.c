ef595eb887074725196e92e53cc7be07090e5246
User10538 <10538@holbertonstudents.com>
2025-04-02 11:16:22 +0000

    Upload files to save

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: const character
+ * Return:the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+
+
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: const character
+ * Return:the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b);
+
+/**
+ * _putchar - to print
+ * @c: get the character
+ * Return: nothing.
+ */
+int _putchar(char c); /**this is for putchar*/
+
+#endif /* MAIN_H */
