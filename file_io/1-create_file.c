30d7943592dd89a03766ee892a85b94c2376e9dd
User10538 <10538@holbertonstudents.com>
2025-04-04 14:47:49 +0000

    Fix  error

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,9 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (filename == NULL)
+                return (-1);
+
 	if (fd == -1)
 		return (-1);
 	
@@ -30,9 +33,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-
-	if (filename == NULL)
-		return (-1);
 
 	close(fd);
 	return (-1);
