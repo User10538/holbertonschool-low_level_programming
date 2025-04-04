197b47571883d5c1896ebb6e6e41284a8ceeb229
User10538 <10538@holbertonstudents.com>
2025-04-04 14:40:52 +0000

    Fix Betty error

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,12 +11,11 @@
  * @text_content: char
  * Return: 1 on success, -1 on failure
  */
- 
- int create_file(const char *filename, char *text_content)
- {
+
+int create_file(const char *filename, char *text_content)
+{
 	ssize_t bytes_written;
-        
-        int fd = open(filename, O_CREAT | O_WRONLY, 0644); 
+	int fd = open(filename, O_CREAT | O_WRONLY, 0644);
 
 	if (text_content != NULL)
 	{
@@ -24,14 +23,15 @@
 
 		if (bytes_written == -1)
 		{
-			close (fd);
+			close(fd);
 			return (-1);
 		}
 	}
+
 	if (filename == NULL)
-		return ( -1);
+		return (-1);
 
 	close(fd);
 	return (-1);
- }
+}
 
