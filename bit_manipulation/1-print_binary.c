701159bca1689c42e50d063d84333a765acbb492
User10538 <10538@holbertonstudents.com>
2025-04-04 02:44:16 +0000

    Binary of int

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: long int
+ */
+void print_binary(unsigned long int n)
+{
+	int bit_position, started = 0;
+
+	for (bit_position = sizeof (n) * 8 -1; bit_position >= 0; bit_position --)
+	{
+		if ((n >> bit_position) & 1)
+		{
+			_putchar('1');
+			started = 1;
+		}
+		else if (started)
+		{
+			_putchar('0');
+		}
+	}
+
+	
+}
+
