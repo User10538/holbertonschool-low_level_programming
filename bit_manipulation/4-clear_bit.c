5054f3bcaf72271c78ec06024c39dbb2f0e3ed3a
User10538 <10538@holbertonstudents.com>
2025-04-04 08:36:50 +0000

    Clear bits

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+/**
+ * clear_bit -  function that sets the value of a bit to 0 at a given index.
+ * @n: long int
+ * @index: unsigned int
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int range = sizeof(n) * 8;
+
+	/*use bitwise AND and NOT to clear the bits*/
+	unsigned long int mask = ~(1UL << index);
+	/*check if index out of range*/
+	if (index >= range)
+		return (-1);
+
+	/*set bit at index to 0 using bitwise OR*/
+	*n &= mask;
+	return (0);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -6,6 +6,15 @@
 #include <stdio.h>
 
 /**
+ * set_bit -  function that sets the value of a bit to 0 at a given index.
+ *
+ * @n: long int
+ * @index: unsigned int
+ * Return - 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/**
  * set_bit - function that sets the value of a bit to 1 at a given in
  * @n: long int
  * @index: unsigned int
