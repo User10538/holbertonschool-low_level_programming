d329fde651434cc267ddddb949a0260fadfbaaca
User10538 <10538@holbertonstudents.com>
2025-04-06 10:05:34 +0000

    Test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,34 +15,33 @@
  */
 void error_exit(int code, const char *message, const char *arg)
 {
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
+    dprintf(STDERR_FILENO, message, arg);
+    exit(code);
 }
 
-
 /*
- * copy_file_to_file - Copies the contents of one file to another
+ * copy_file_to_file - file to file
  * @file_from: source file
  * @file_to: destination file
- *
- * Return: 1 on success, or -1 on error
  */
 int copy_file_to_file(const char *file_from, const char *file_to)
 {
-	int fd_to, fd_from;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+        int fd_to, fd_from;
+        ssize_t bytes_read, bytes_written;
+        char buffer[BUFFER_SIZE];
 
+        /*open the file_from*/
         fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		error_exit (98, "Error: Can't write to %s\n", file_from);
+        if (fd_from == -1)
+                error_exit (98, "Error: Can't write to %s\n", file_from);
 
+        /*can not create or if write to file_to fails*/
         fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if(fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", file_to);
-	}
+        if(fd_to == -1)
+        {
+                close(fd_from);
+                error_exit(99, "Error: Can't write to %s\n", file_to);
+        }
 
         /* Read and write loop */
         while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -63,11 +62,11 @@ int copy_file_to_file(const char *file_from, const char *file_to)
                 error_exit(98, "Error: Can't read from  %s\n", file_from);
 
         }
-	
-	if (bytes_read == 0)
+
+        /* Handle EOF (bytes_read == 0) */
+        if (bytes_read == 0)
         {
-		close(fd_from);
-		close(fd_to);
+                /* Successfully reached EOF, no need to handle further */
         }
 
 
@@ -80,13 +79,6 @@ int copy_file_to_file(const char *file_from, const char *file_to)
         return (1);
 }
 
-/**
- * main - Copies the content of one file to another
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 on success, or an exit code on error
- */
 int main(int argc, char *argv[])
 {
     if (argc != 3)  /* Check if the number of arguments is correct */
