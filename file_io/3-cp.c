7f20dc56079e9ba1a5219e803e46261788b011e3
User10538 <10538@holbertonstudents.com>
2025-04-06 11:20:47 +0000

    Test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,21 +5,16 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * main - to copy file
- * @argc: intger
- * @argv: char
- * Return: 0 on success, or exits with appropriate code on failure
- */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, w, r;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
+	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
@@ -35,18 +30,21 @@ int main(int argc, char *argv[])
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from);
 		exit(99);
+	}
 	while (r > 0)
 	{
 		w = write(file_to, buffer, r);
 		if (w == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(file_from);
 			close(file_to);
 			exit(99);
-
+		}
 		r = read(file_from, buffer, BUFFER_SIZE);
 		if (r == -1)
 		{
@@ -57,8 +55,10 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (close(file_from) == -1 || close(file_to) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
 		exit(100);
+	}
 	return (0);
 }
 
