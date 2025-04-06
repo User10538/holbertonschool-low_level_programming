28ffbe4d00112500c799d38c4ca7db41d60445f4
User10538 <10538@holbertonstudents.com>
2025-04-06 10:50:11 +0000

    Test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,71 +5,35 @@
 
 #define BUFFER_SIZE 1024
 
+void error_exit(int fd, int err_code, char *msg)
+{
+	if (fd != -1) close(fd);
+	dprintf(STDERR_FILENO, "Error: %s\n", msg);
+	exit(err_code);
+}
+
 int main(int argc, char *argv[])
 {
-    int file_from, file_to, w, r;
+    int file_from, file_to, r, w;
     char buffer[BUFFER_SIZE];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+    if (argc != 3) error_exit(-1, 97, "Usage: cp file_from file_to");
 
-    /** Open file_from for reading **/
     file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+    if (file_from == -1) error_exit(file_from, 98, "Can't read from file");
 
-    /** Read from file_from **/
-    r = read(file_from, buffer, BUFFER_SIZE);
-    if (r == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close(file_from);
-        exit(98);
-    }
-
-    /** Open file_to for writing, create if needed, truncate if exist **/
     file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(file_from);
-        exit(99);
-    }
+    if (file_to == -1) error_exit(file_from, 99, "Can't write to file");
 
-    /** Copy content from file_from to file_to **/
-    while (r > 0)
+    while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
     {
         w = write(file_to, buffer, r);
-        if (w == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            close(file_from);
-            close(file_to);
-            exit(99);
-        }
-
-        r = read(file_from, buffer, BUFFER_SIZE);
-        if (r == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-            close(file_from);
-            close(file_to);
-            exit(98);
-        }
+        if (w == -1) error_exit(file_from, 99, "Can't write to file");
     }
+    if (r == -1) error_exit(file_from, 98, "Can't read from file");
 
-    /** Close file descriptors and handle any errors **/
     if (close(file_from) == -1 || close(file_to) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-        exit(100);
-    }
+        error_exit(-1, 100, "Can't close fd");
 
     return 0;
 }
---
+++ file_io/4-cp.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, w, r;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	r = read(file_from, buffer, BUFFER_SIZE);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	while (r > 0)
+	{
+		w = write(file_to, buffer, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+
+		r = read(file_from, buffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(file_from);
+			close(file_to);
+			exit(98);
+		}
+	}
+
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
+	return (0);
+}
+
